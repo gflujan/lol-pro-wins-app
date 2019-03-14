c9a01801c5fd05c1cec8698967e19ab5d2b121e2
Gabriel F. Lujan <gfl.devo@gmail.com>
2019-03-14 15:32:37 -0700

    Update pro players file/object with 2 years of on-stage wins

--- src/static_data/lcs_pro_players.js
+++ src/static_data/lcs_pro_players.js
@@ -1,82 +1,152 @@
-export const lcs = {
-  '100': {
-    "Ssumday":      { position: 'top' },
-    'AnDa':         { position: 'jungle' },
-    'Huhi':         { position: 'mid' },
-    'Bang':         { position: 'bot' },
-    'Aphromoo':     { position: 'supp' },
-    'Pr0lly':       { position: 'coach' },
+export const lcsTeams = {
+  t100: {
+    teamName: '100 Thieves',
+    teamWins: {
+      '2019': { spring: 4, summer: 0 },
+      '2018': { spring: 12, summer: 10 },
+      '2017': { spring: null, summer: null },
+    },
+      top:  'Ssumday',
+       jg:  'AnDa',
+      mid:  'Huhi',
+      bot:  'Bang',
+      sup:  'Aphromoo',
+    coach:  'Pr0lly',
   },
-  'c9': {
-    'Licorice':     { position: 'top' },
-    'Svenskeren':   { position: 'jungle' },
-    'Nisqy':        { position: 'mid' },
-    'Sneaky':       { position: 'bot' },
-    'Zeyzal':       { position: 'supp' },
-    'Reapered':     { position: 'coach' },
+  c9: {
+    teamName: 'Cloud 9',
+    teamWins: {
+      '2019': { spring: 11, summer: 0 },
+      '2018': { spring: 11, summer: 11 },
+      '2017': { spring: 14, summer: 12 },
+    },
+      top:  'Licorice',
+       jg:  'Svenskeren',
+      mid:  'Nisqy',
+      bot:  'Sneaky',
+      sup:  'Zeyzal',
+    coach:  'Reapered',
   },
-  'cg': {
-    'Huni': { position: 'top' },
-    'Lira': { position: 'jungle' },
-    'Damonte': { position: 'mid' },
-    'Piglet': { position: 'bot' },
-    'Vulcan': { position: 'supp' },
-    'McScrag': { position: 'coach' },
+  cg: {
+    teamName: 'Clutch Gaming',
+    teamWins: {
+      '2019': { spring: 5, summer: 0 },
+      '2018': { spring: 11, summer: 6 },
+      '2017': { spring: null, summer: null },
+    },
+      top:  'Huni',
+       jg:  'Lira',
+      mid:  'Damonte',
+      bot:  'Piglet',
+      sup:  'Vulcan',
+    coach:  'McScrag',
   },
-  'clg': {
-    'Darshan': { position: 'top' },
-    'Wiggily': { position: 'jungle' },
-    'PowerOfEvil': { position: 'mid' },
-    'Stixxay': { position: 'bot' },
-    'Biofrost': { position: 'supp' },
-    'Weldon': { position: 'coach' },
+  clg: {
+    teamName: 'Counter Logic Gaming',
+    teamWins: {
+      '2019': { spring: 5, summer: 0 },
+      '2018': { spring: 7, summer: 7 },
+      '2017': { spring: 10, summer: 12 },
+    },
+      top:  'Darshan',
+       jg:  'Wiggily',
+      mid:  'PowerOfEvil',
+      bot:  'Stixxay',
+      sup:  'Biofrost',
+    coach:  'Weldon',
   },
-  'fox': {
-    'Solo': { position: 'top' },
-    'Rush': { position: 'jungle' },
-    'Fenix': { position: 'mid' },
-    'Apollo': { position: 'bot' },
-    'Hakuho': { position: 'supp' },
-    'Ssong': { position: 'coach' },
+  fox: {
+    teamName: 'Echo Fox',
+    teamWins: {
+      '2019': { spring: 4, summer: 0 },
+      '2018': { spring: 12, summer: 10 },
+      '2017': { spring: 6, summer: 5 },
+    },
+      top:  'Solo',
+       jg:  'Rush',
+      mid:  'Fenix',
+      bot:  'Apollo',
+      sup:  'Hakuho',
+    coach:  'Ssong',
   },
-  'fly': {
-    'V1per': { position: 'top' },
-    'Santorin': { position: 'jungle' },
-    'Pobelter': { position: 'mid' },
-    'WildTurtle': { position: 'bot' },
-    'JayJ': { position: 'supp' },
-    'Invert': { position: 'coach' },
+  fly: {
+    teamName: 'FlyQuest',
+    teamWins: {
+      '2019': { spring: 7, summer: 0 },
+      '2018': { spring: 6, summer: 10 },
+      '2017': { spring: 9, summer: 6 },
+    },
+      top:  'V1per',
+       jg:  'Santorin',
+      mid:  'Pobelter',
+      bot:  'WildTurtle',
+      sup:  'JayJ',
+    coach:  'Invert',
   },
-  'ggs': {
-    'Hauntzer': { position: 'top' },
-    'Contractz': { position: 'jungle' },
-    'Froggen': { position: 'mid' },
-    'Deftly': { position: 'bot' },
-    'Olleh': { position: 'supp' },
-    'Inero': { position: 'coach' },
+  ggs: {
+    teamName: 'Golden Guardians',
+    teamWins: {
+      '2019': { spring: 6, summer: 0 },
+      '2018': { spring: 4, summer: 5 },
+      '2017': { spring: null, summer: null },
+    },
+      top:  'Hauntzer',
+       jg:  'Contractz',
+      mid:  'Froggen',
+      bot:  'Deftly',
+      sup:  'Olleh',
+    coach:  'Inero',
   },
-  'opt': {
-    'Dhokla': { position: 'top' },
-    'Meteos': { position: 'jungle' },
-    'Crown': { position: 'mid' },
-    'Arrow': { position: 'bot' },
-    'Big': { position: 'supp' },
-    'Zaboutine': { position: 'coach' },
+  opt: {
+    teamName: 'Optic Gaming',
+    teamWins: {
+      '2019': { spring: 6, summer: 0 },
+      '2018': { spring: 5, summer: 9 },
+      '2017': { spring: null, summer: null },
+    },
+      top:  'Dhokla',
+       jg:  'Meteos',
+      mid:  'Crown',
+      bot:  'Arrow',
+      sup:  'Big',
+    coach:  'Zaboutine',
   },
-  'tl': {
-    'Impact': { position: 'top' },
-    'Xmithie': { position: 'jungle' },
-    'Jensen': { position: 'mid' },
-    'Doublelift': { position: 'bot' },
-    'CoreJJ': { position: 'supp' },
-    'Cain': { position: 'coach' },
+  tl: {
+    teamName: 'Team Liquid',
+    teamWins: {
+      '2019': { spring: 13, summer: 0 },
+      '2018': { spring: 11, summer: 12 },
+      '2017': { spring: 5, summer: 4 },
+    },
+      top:  'Impact',
+       jg:  'Xmithie',
+      mid:  'Jensen',
+      bot:  'Doublelift',
+      sup:  'CoreJJ',
+    coach:  'Cain',
   },
-  'tsm': {
-    'Broken Blade': { position: 'top' },
-    'Akaadian': { position: 'jungle' },
-    'Bjergsen': { position: 'mid' },
-    'Zven': { position: 'bot' },
-    'Smoothie': { position: 'supp' },
-    'Zikz': { position: 'coach' },
+  tsm: {
+    teamName: 'Team SoloMid',
+    teamWins: {
+      '2019': { spring: 9, summer: 0 },
+      '2018': { spring: 11, summer: 10 },
+      '2017': { spring: 15, summer: 14 },
+    },
+      top:  'Broken Blade',
+       jg:  'Akaadian',
+      mid:  'Bjergsen',
+      bot:  'Zven',
+      sup:  'Smoothie',
+    coach:  'Zikz',
   },
 };
+
+/* ---------------------------------------------
+// LINKS
+--------------------------------------------- */
+// https://www.esportspedia.com/lol/LCS_2019_Spring
+// https://www.esportspedia.com/lol/LCS_2019_Summer
+// https://www.esportspedia.com/lol/NA_LCS_2018_Spring_Split
+// https://www.esportspedia.com/lol/NA_LCS_2018_Summer_Split
+// https://www.esportspedia.com/lol/NA_LCS_2017_Spring_Split
+// https://www.esportspedia.com/lol/NA_LCS_2017_Summer_Split
